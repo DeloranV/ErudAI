a5298177ebdf9a807089e977cc99910713a7b95c
DeloranV <34947157+DeloranV@users.noreply.github.com>
2025-04-23 22:45:44 +0200

    initial commit + stylesheet sample

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+import os
 from PySide6.QtWidgets import QApplication
 from view import ChatDialog
 
 
 if __name__ == "__main__":
+    stylesheet_path = os.path.join('view', 'static', 'style.qss')
+
     app = QApplication()
+
     chat_dialog = ChatDialog()
     chat_dialog.show()
+
+    with open(stylesheet_path, 'r') as stylesheet_file:
+        _style = stylesheet_file.read()
+        chat_dialog.setStyleSheet(_style)
+        print(_style)
+
     app.exec()
