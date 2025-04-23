667168382414fd41849760e8da26f452d6901914
DeloranV <34947157+DeloranV@users.noreply.github.com>
2025-04-23 23:24:42 +0200

    Integrated GUI with the querying system

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from PySide6.QtWidgets import QApplication
 from view import ChatDialog
-
+from agent import ActionPerformer, Query
+from rag import GraphRetriever
 
 if __name__ == "__main__":
+    n4j_auth_data = open("n4j_auth_data", "r").read().split("\n")
+    hf_api_key = open("hf_api_key", "r").read()
+
     app = QApplication()
-    chat_dialog = ChatDialog()
+    chat_dialog = ChatDialog(n4j_uri=n4j_auth_data[0],
+                             n4j_auth=(n4j_auth_data[1], n4j_auth_data[2]),
+                             n4j_db_name="neo4j",
+                             endpoint_url="https://fn7lxcome3tixe20.us-east-1.aws.endpoints.huggingface.cloud/v1/",
+                             endpoint_api_key=hf_api_key
+                             )
     chat_dialog.show()
     app.exec()
