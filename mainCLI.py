03920b41001eb47a2a3777b0cb4f303cd84379ee
DeloranV <34947157+DeloranV@users.noreply.github.com>
2025-04-23 16:09:12 +0200

    added graph retriever

--- mainCLI.py
+++ mainCLI.py
@@ -1,11 +1,17 @@
 from util import ImageEncoder, Snapshotter
 from agent import ActionPerformer, Query
+from rag import GraphRetriever
 
 snapshotter = Snapshotter()
 image_encoder = ImageEncoder()
 action_performer = ActionPerformer()
 
-api_key = open("api_key", "r").read()
+n4j_auth_data = open("n4j_auth_data", "r").read().split("\n")
+hf_api_key = open("hf_api_key", "r").read()
+
+graph = GraphRetriever(n4j_auth_data[0], (n4j_auth_data[1], n4j_auth_data[2]), "neo4j")
+graph.test_connectivity()
+context_var = graph.get_ui_context()
 
 query = Query(prompt="Close browser window",
               debug=True)
@@ -13,7 +19,7 @@ query = Query(prompt="Close browser window",
 encoded = image_encoder.encode(snapshotter.snapshot())
 
 result = query.send(encoded_image=encoded,
-                    api_key=api_key,
+                    api_key=hf_api_key,
                     base_url="https://fn7lxcome3tixe20.us-east-1.aws.endpoints.huggingface.cloud/v1/"
                     )
 
