12d7fd402f1d1efddd01c4ef218e174ad3483fb9
DeloranV <34947157+DeloranV@users.noreply.github.com>
2025-04-24 22:12:17 +0200

    fixed main.py

--- main.py
+++ main.py
@@ -10,12 +10,9 @@ if __name__ == "__main__":
 
     app = QApplication()
 
-    chat_dialog = ChatDialog()
-
     n4j_auth_data = open("n4j_auth_data", "r").read().split("\n")
     hf_api_key = open("hf_api_key", "r").read()
 
-    app = QApplication()
     chat_dialog = ChatDialog(n4j_uri=n4j_auth_data[0],
                              n4j_auth=(n4j_auth_data[1], n4j_auth_data[2]),
                              n4j_db_name="neo4j",
@@ -28,6 +25,5 @@ if __name__ == "__main__":
     with open(stylesheet_path, 'r') as stylesheet_file:
         _style = stylesheet_file.read()
         chat_dialog.setStyleSheet(_style)
-        print(_style)
 
     app.exec()
